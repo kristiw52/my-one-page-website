9fba35895d98b8462b3574eac3556fa0651ebd19
Kristi Wright <kristiw52@hotmail.com>
2016-11-29 19:06:12 -0800

    updating repo name

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// JavaScript Document
+// Set up the nav scrolling effect
+$("nav").onePageNav();
+
+var menu = $("nav ul");
+
+// Toggle the menu when clicked
+$("#menu-icon").click(function() {
+	menu.slideToggle();
+});
+
+// Fix bug where menu disappears if you open and close it on a small screen
+// When I resize the window
+$(window).resize(function() {
+	var width = $(window).width();
+
+	// if window width is > 768 and the menu is hidden
+	if(width > 768 && menu.is(":hidden")) {
+		menu.removeAttr("style");
+	}
+
+});
+
+// Add parallax (stellar.js)
+$.stellar();
+
+// Add wow.js animations
+new WOW().init();
+
+
+
+/*  This is the navigation wrapper
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if($(window).scrollTop() > 900 &&  
+      $(".nav-wrapper").css("position") != "fixed"){
+      $(".nav-wrapper").hide(function(){
+        $(".nav-wrapper").css("position", "fixed")
+        $(".nav-wrapper").slideDown('1000')
+      })
+    }else if($(window).scrollTop() <= 900 ){
+      $(".nav-wrapper").css("position", "initial")
+    }
+  })
+})
+*/
