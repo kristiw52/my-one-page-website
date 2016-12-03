05742832d53c06115849ddb8f6082e11262ce3dd
Christopher Baek <christopher.baek@gmail.com>
2016-12-02 16:24:29 -0800

    Finish cleaning up formatting
    - Replace tabs with appropriate spacing (2 in HTML and JS, 4 in CSS) for this project
    - Remove trailing spaces
    - Clean up indentation

--- js/main.js
+++ js/main.js
@@ -6,18 +6,18 @@ var menu = $("nav ul");
 
 // Toggle the menu when clicked
 $("#menu-icon").click(function() {
-	menu.slideToggle();
+  menu.slideToggle();
 });
 
 // Fix bug where menu disappears if you open and close it on a small screen
 // When I resize the window
 $(window).resize(function() {
-	var width = $(window).width();
+  var width = $(window).width();
 
-	// if window width is > 768 and the menu is hidden
-	if(width > 768 && menu.is(":hidden")) {
-		menu.removeAttr("style");
-	}
+  // if window width is > 768 and the menu is hidden
+  if(width > 768 && menu.is(":hidden")) {
+    menu.removeAttr("style");
+  }
 
 });
 
